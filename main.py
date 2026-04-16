58f87365f2494c6ccf050e6a832edcd21d566c62
ViniciusSOAlves <149978269+ViniciusSOAlves@users.noreply.github.com>
2026-04-16 15:31:15 -0300

    Limpeza de dados nas bases SINASC e SIM

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pandas as pd
 def carregar_dados(caminho_sinasc, caminho_sim):
     print("Carregando bases...")
 
-    # 1. Tente ler apenas a primeira linha para conferir os nomes reais das colunas
-    # Isso ajuda a diagnosticar se o problema é o separador ou o nome
+   
+  
     temp_df = pd.read_csv(caminho_sinasc, nrows=1, sep=None, engine='python')
     print("Colunas encontradas no arquivo:", temp_df.columns.tolist())
 
@@ -13,23 +13,19 @@ def carregar_dados(caminho_sinasc, caminho_sim):
         'PESO', 'SEMAGESTAC', 'RACACOR', 'GRAVIDEZ',
         'PARTO', 'IDADEMAE', 'SEXO', 'DTNASC', 'CODMUNNASC', 'APGAR5', 'IDANOMAL'
     ]
-
-    # 2. Adicionamos 'sep=None' e 'engine=python' para o Pandas detectar
-    # automaticamente se o separador é vírgula ou ponto e vírgula.
     sinasc = pd.read_csv(caminho_sinasc, usecols=colunas_necessarias, sep=None, engine='python')
     sim = pd.read_csv(caminho_sim, sep=None, engine='python')
 
     return sinasc, sim
 
-# 2. Pré-processamento do Baseline [cite: 190, 481]
+# 2. Pré-processamento do Baseline
 def pre_processamento_baseline(df_sinasc):
     print("Iniciando limpeza de outliers e nulos...")
     
-    # Removendo valores nulos na coluna PESO [cite: 482]
+    # Removendo valores nulos na coluna PESO 
     df_limpo = df_sinasc.dropna(subset=['PESO']).copy()
     
-    # Definindo outliers (Exemplo baseado no TCC: remover valores extremos)
-    # No TCC foram removidos 162.981 outliers [cite: 482]
+    # Definindo outliers
     q_low = df_limpo['PESO'].quantile(0.01)
     q_hi  = df_limpo['PESO'].quantile(0.99)
     
